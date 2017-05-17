02dadf5e74c80f1b8e483c9dc2afa1e4c3463eea
Vlad-Alexandru Tănăsescu <vlad_tanasescu@outlook.com>
2017-05-17 11:38:21 +0100

    Commit Alpha

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace DupeTracker
+{
+    partial class DupeTracker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(943, 451);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Doppelgänger";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(882, 482);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(12, 482);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(864, 50);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textBox1.Location = new System.Drawing.Point(59, 453);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(817, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "^\\s*<item code=\"(?<code>\\d*)\">(?<label>.*)<\\/item>$";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_FocusEnter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_FocusLeave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 456);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Regex:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textBox3.Location = new System.Drawing.Point(12, 12);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(1027, 435);
+            this.textBox3.TabIndex = 7;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.ForestGreen;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.DimGray;
+            this.linkLabel1.Location = new System.Drawing.Point(940, 428);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(78, 13);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Export Console";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.DimGray;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DupeTracker.Properties.Resources.warning;
+            this.pictureBox1.Location = new System.Drawing.Point(882, 452);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            this.toolTip1.SetToolTip(this.pictureBox1, "Currently, the regular expression does not support more than two expression group" +
+        "s!");
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 5000;
+            this.toolTip1.InitialDelay = 100;
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ReshowDelay = 100;
+            // 
+            // DupeTracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 544);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "DupeTracker";
+            this.Text = "DupeTracker";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace DupeTracker
+{
+    public partial class DupeTracker : Form
+    {
+        string CurrentPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location).ToString();
+
+        public DupeTracker()
+        {
+            InitializeComponent();
+            label1.Text = CurrentPath + "...";
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            Console.SetOut(new TextBoxWriter(textBox3));
+            Console.WriteLine("Welcome to DupeTracker." + "\n" + "Click Doppelgänger to find duplicate entries in clinical forms located in the selected directory." + "\n");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                CurrentPath = folderBrowserDialog1.SelectedPath;
+                label1.Text = CurrentPath + "...";
+                Console.WriteLine("Path has changed to " + label1.Text + "\n");
+            }
+        }
+
+        private void textBox1_FocusEnter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "^\\s*<item code=\"(?<code>\\d*)\">(?<label>.*)<\\/item>$")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = SystemColors.ActiveCaptionText;
+            }
+        }
+
+        private void textBox1_FocusLeave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "^\\s*<item code=\"(?<code>\\d*)\">(?<label>.*)<\\/item>$";
+                textBox1.ForeColor = SystemColors.ControlDarkDark;
+            }
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.Help;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int fCount = Directory.GetFiles(CurrentPath, "*.xml", SearchOption.TopDirectoryOnly).Length;
+            if (fCount > 0)
+            {
+                string CurrentLine;
+                string CompareLine;
+
+                string RegEx = textBox1.Text;
+
+                foreach (string file in Directory.EnumerateFiles(CurrentPath, "*.xml"))
+                {
+                    Console.WriteLine(file);
+                    var lines = File.ReadLines(file);
+                    CurrentLine = lines.First();
+
+                    for (int i = 1; i <= lines.Count(); i++)
+                    {
+                        Match iMatch = Regex.Match(CurrentLine, RegEx);
+                        if (iMatch.Groups[1].Value.Length >= 0)
+                        {
+                            for (int j = i + 1; j <= lines.Count(); j++)
+                            {
+                                CompareLine = lines.Skip(j - 1).First();
+                                Match jMatch = Regex.Match(CompareLine, RegEx);
+                                if (jMatch.Groups[1].Value.Length > 0)
+                                    if (iMatch.Groups[1].Value == jMatch.Groups[1].Value)
+                                        if (!textBox3.Text.Contains(jMatch.Groups[2].Value))
+                                        Console.WriteLine("Code " + iMatch.Groups[1] + " with the label \"" + iMatch.Groups[2] + "\" on line " + i + " is duplicated by code " + jMatch.Groups[1] + " with the label \"" + jMatch.Groups[2] + "\" on line " + j + ".");
+                                if (i != lines.Count())
+                                    CurrentLine = lines.Skip(i).First();
+                            }
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+                Console.WriteLine("No XML files found in the selected directory!" + "\n");
+        }
+    }
+}
